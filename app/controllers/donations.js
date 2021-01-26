a5914be6c2ec6488a2244362f116197535601efe
Eamonn de Leastar <edeleastar@wit.ie>
2021-01-26 10:24:08 +0000

    routes to trigger new views

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -4,6 +4,16 @@ const Donations = {
       return h.file("./app/views/main.html");
     },
   },
+  signup: {
+    handler: function (request, h) {
+      return h.file("./app/views/signup.html");
+    },
+  },
+  login: {
+    handler: function (request, h) {
+      return h.file("./app/views/login.html");
+    },
+  },
 };
 
 module.exports = Donations;
