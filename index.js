266a35bf730f15291e77b6a4c448dd4dc548cffa
Eamonn de Leastar <edeleastar@wit.ie>
2021-02-04 08:52:01 +0000

    change import signature

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Hapi = require("@hapi/hapi");
 const Inert = require("@hapi/inert");
 const Vision = require("@hapi/vision");
 const Handlebars = require("handlebars");
+const Cookie = require("@hapi/cookie");
 
 const server = Hapi.server({
   port: 3000,
@@ -19,7 +20,7 @@ server.bind({
 async function init() {
   await server.register(Inert);
   await server.register(Vision);
-  await server.register(require("@hapi/cookie"));
+  await server.register(Cookie);
   server.views({
     engines: {
       hbs: require("handlebars"),
