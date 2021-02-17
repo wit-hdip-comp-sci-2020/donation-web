e083730c6e9143b5ff4f33a10fd649701615ab5c
Eamonn de Leastar <edeleastar@wit.ie>
2021-02-17 09:23:37 +0000

    catch database errors - and redirect to main

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -4,33 +4,37 @@ const User = require("../models/user");
 
 const Donations = {
   home: {
-    handler: function (request, h) {
+    handler: function(request, h) {
       return h.view("home", { title: "Make a Donation" });
-    },
+    }
   },
   report: {
-    handler: async function (request, h) {
+    handler: async function(request, h) {
       const donations = await Donation.find().populate("donor").lean();
       return h.view("report", {
         title: "Donations to Date",
-        donations: donations,
+        donations: donations
       });
-    },
+    }
   },
   donate: {
-    handler: async function (request, h) {
-      const id = request.auth.credentials.id;
-      const user = await User.findById(id);
-      const data = request.payload;
-      const newDonation = new Donation({
-        amount: data.amount,
-        method: data.method,
-        donor: user._id,
-      });
-      await newDonation.save();
-      return h.redirect("/report");
-    },
-  },
+    handler: async function(request, h) {
+      try {
+        const id = request.auth.credentials.id;
+        const user = await User.findById(id);
+        const data = request.payload;
+        const newDonation = new Donation({
+          amount: data.amount,
+          method: data.method,
+          donor: user._id
+        });
+        await newDonation.save();
+        return h.redirect("/report");
+      } catch (err) {
+        return h.view("main", { errors: [{ message: err.message }] });
+      }
+    }
+  }
 };
 
 module.exports = Donations;
