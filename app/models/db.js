c821a4fbc8cccbe38c09b5ad6bc67b3933d4287e
Eamonn de Leastar <edeleastar@wit.ie>
2021-03-03 10:08:11 +0000

    load the candidate model + seed data

--- app/models/db.js
+++ app/models/db.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const env = require('dotenv');
+const env = require("dotenv");
 env.config();
 
 const Mongoose = require("mongoose");
@@ -11,14 +11,25 @@ Mongoose.set("useUnifiedTopology", true);
 Mongoose.connect(process.env.db);
 const db = Mongoose.connection;
 
-db.on("error", function(err) {
+async function seed() {
+  var seeder = require("mais-mongoose-seeder")(Mongoose);
+  const data = require("./seed-data.json");
+  const Donation = require("./donation");
+  const Candidate = require("./candidate.js");
+  const User = require("./user");
+  const dbData = await seeder.seed(data, { dropDatabase: false, dropCollections: true });
+  console.log(dbData);
+}
+
+db.on("error", function (err) {
   console.log(`database connection error: ${err}`);
 });
 
-db.on("disconnected", function() {
+db.on("disconnected", function () {
   console.log("database disconnected");
 });
 
-db.once("open", function() {
+db.once("open", function () {
   console.log(`database connected to ${this.name} on ${this.host}`);
+  seed();
 });
