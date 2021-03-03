176e00997edf4a2a76e07ed2520a05b45a448195
Eamonn de Leastar <edeleastar@wit.ie>
2021-03-03 10:09:31 +0000

    support candidate in controller + views

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -1,40 +1,50 @@
 "use strict";
 const Donation = require("../models/donation");
 const User = require("../models/user");
+const Candidate = require("../models/candidate");
 
 const Donations = {
   home: {
-    handler: function(request, h) {
-      return h.view("home", { title: "Make a Donation" });
-    }
+    handler: async function (request, h) {
+      const candidates = await Candidate.find().lean();
+      return h.view("home", { title: "Make a Donation", candidates: candidates });
+    },
   },
   report: {
-    handler: async function(request, h) {
-      const donations = await Donation.find().populate("donor").lean();
+    handler: async function (request, h) {
+      const donations = await Donation.find().populate("donor").populate("candidate").lean();
       return h.view("report", {
         title: "Donations to Date",
-        donations: donations
+        donations: donations,
       });
-    }
+    },
   },
   donate: {
-    handler: async function(request, h) {
+    handler: async function (request, h) {
       try {
         const id = request.auth.credentials.id;
         const user = await User.findById(id);
         const data = request.payload;
+
+        const rawCandidate = request.payload.candidate.split(",");
+        const candidate = await Candidate.findOne({
+          lastName: rawCandidate[0],
+          firstName: rawCandidate[1],
+        });
+
         const newDonation = new Donation({
           amount: data.amount,
           method: data.method,
-          donor: user._id
+          donor: user._id,
+          candidate: candidate._id,
         });
         await newDonation.save();
         return h.redirect("/report");
       } catch (err) {
         return h.view("main", { errors: [{ message: err.message }] });
       }
-    }
-  }
+    },
+  },
 };
 
 module.exports = Donations;
