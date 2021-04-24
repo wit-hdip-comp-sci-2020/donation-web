d9d5b8856644fe62e78d6ce498540ef9c8b0612f
Eamonn de Leastar <edeleastar@wit.ie>
2021-04-24 07:59:35 +0100

    rework tests to support authentication

--- test/donation-api-test.js
+++ test/donation-api-test.js
@@ -8,8 +8,20 @@ const _ = require("lodash");
 suite("Donation API tests", function () {
   let donations = fixtures.donations;
   let newCandidate = fixtures.newCandidate;
+  let newUser = fixtures.newUser;
 
   const donationService = new DonationService(fixtures.donationService);
+
+  suiteSetup(async function () {
+    await donationService.deleteAllUsers();
+    const returnedUser = await donationService.createUser(newUser);
+    const response = await donationService.authenticate(newUser);
+  });
+
+  suiteTeardown(async function () {
+    await donationService.deleteAllUsers();
+    donationService.clearAuth();
+  });
 
   setup(async function () {
     donationService.deleteAllCandidates();
@@ -22,6 +34,7 @@ suite("Donation API tests", function () {
     const returnedCandidate = await donationService.createCandidate(newCandidate);
     await donationService.makeDonation(returnedCandidate._id, donations[0]);
     const returnedDonations = await donationService.getDonations(returnedCandidate._id);
+    console.log(returnedDonations);
     assert.equal(returnedDonations.length, 1);
     assert(_.some([returnedDonations[0]], donations[0]), "returned donation must be a superset of donation");
   });
