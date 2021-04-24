314b707212885822ccfbd2ce3eef8ba6a83b8f47
Eamonn de Leastar <edeleastar@wit.ie>
2021-04-24 07:41:21 +0100

    equip DonationService with authentication support

--- test/donation-service.js
+++ test/donation-service.js
@@ -123,6 +123,20 @@ class DonationService {
       return null;
     }
   }
+
+  async authenticate(user) {
+    try {
+      const response = await axios.post(this.baseUrl + "/api/users/authenticate", user);
+      axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.token;
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async clearAuth(user) {
+    axios.defaults.headers.common["Authorization"] = "";
+  }
 }
 
 module.exports = DonationService;
