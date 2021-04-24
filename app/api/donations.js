42025f8776bd3c2d39b43a56089f7a09665942c9
Eamonn de Leastar <edeleastar@wit.ie>
2021-04-24 08:06:13 +0100

    support and test donor in donation endpoint

--- app/api/donations.js
+++ app/api/donations.js
@@ -3,6 +3,7 @@
 const Donation = require("../models/donation");
 const Candidate = require("../models/candidate");
 const Boom = require("@hapi/boom");
+const utils = require("./utils.js");
 
 const Donations = {
   findAll: {
@@ -23,17 +24,20 @@ const Donations = {
       return donations;
     },
   },
+
   makeDonation: {
     auth: {
       strategy: "jwt",
     },
     handler: async function (request, h) {
+      const userId = utils.getUserIdFromRequest(request);
       let donation = new Donation(request.payload);
       const candidate = await Candidate.findOne({ _id: request.params.id });
       if (!candidate) {
         return Boom.notFound("No Candidate with this id");
       }
       donation.candidate = candidate._id;
+      donation.donor = userId;
       donation = await donation.save();
       return donation;
     },
