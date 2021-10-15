bd6e05e43306455a56ad9ec51786ae899b3223d7
Eamonn de Leastar <edeleastar@wit.ie>
2021-10-15 19:22:31 +0100

    update all packages to latest versions

--- app/models/db.js
+++ app/models/db.js
@@ -4,25 +4,15 @@ import * as dotenv from "dotenv";
 import * as data from "./seed-data.json";
 
 import Mongoose from "mongoose";
-import { Donation } from "./donation.js";
-import { Candidate } from "./candidate.js";
-import { User } from "./user.js";
 import seeder from "mais-mongoose-seeder";
 
 export function initDB() {
   dotenv.config();
 
-  Mongoose.set("useNewUrlParser", true);
-  Mongoose.set("useUnifiedTopology", true);
-
   Mongoose.connect(process.env.db);
   const db = Mongoose.connection;
 
   async function seed() {
-    // var seed = seeder(Mongoose);
-    const users = await User.find();
-    const candidates = await Candidate.find();
-    const donations = await Donation.find();
     const dbData = await seeder(Mongoose).seed(data.default, { dropDatabase: false, dropCollections: true });
     console.log(dbData);
   }
